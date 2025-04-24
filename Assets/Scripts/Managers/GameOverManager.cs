8711eedfd3250a7feeae7eba368911600e154095
AmitDeUnityDev <151022556+AmitDeUnityDev@users.noreply.github.com>
2025-04-24 17:06:20 +0300

    BackgroundImage for game over movement effect has added and better fonts for texts has been added as well

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -2,17 +2,20 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using DG.Tweening;
+using UnityEngine.Serialization;
 
 public class GameOverManager : MonoBehaviour
 {
     public static GameOverManager Instance;
 
     [Header("Text & button Positions")]
+    public RectTransform bgImageRect;
     public RectTransform gameOverRect;
     public RectTransform finalScoreRect;
     public Button replayButtonRect;
 
-    [Header("Movement settings")]
+    [Header("Movement settings")] 
+    public Vector2 bgImageTargetPos;
     public Vector2 gameOverTargetPos;
     public Vector2 scoreTargetPos;
     public float moveDuration = 1f;
@@ -23,7 +26,8 @@ public class GameOverManager : MonoBehaviour
     void Awake()
     {
         Instance = this;
-
+        
+        bgImageRect.gameObject.SetActive(false);
         gameOverRect.gameObject.SetActive(false);
         finalScoreRect.gameObject.SetActive(false);
         replayButtonRect.gameObject.SetActive(false);
@@ -34,27 +38,34 @@ public class GameOverManager : MonoBehaviour
         if (IsGameOver) return;
         IsGameOver = true;
         
-        // show & animate Game Over
-        gameOverRect.anchoredPosition = new Vector2(0, -Screen.height);
-        gameOverRect.gameObject.SetActive(true);
-        gameOverRect.DOAnchorPos(gameOverTargetPos, moveDuration)
+        bgImageRect.anchoredPosition = new Vector2(0, -Screen.height);
+        bgImageRect.gameObject.SetActive(true);
+        bgImageRect.DOAnchorPos(bgImageTargetPos, moveDuration)
             .SetEase(Ease.OutBack)
             .OnComplete(() =>
             {
-                // show & animate score
-                DOVirtual.DelayedCall(delayBetween, () =>
-                {
-                    var scoreText = finalScoreRect.GetComponent<TextMeshProUGUI>();
-                    if (scoreText != null) scoreText.text = $"Score: {finalScore}";
-                    finalScoreRect.anchoredPosition = new Vector2(0, -Screen.height);
-                    finalScoreRect.gameObject.SetActive(true);
-                    finalScoreRect.DOAnchorPos(scoreTargetPos, moveDuration)
-                        .SetEase(Ease.OutBack)
-                        .OnComplete(() =>
+                // show & animate Game Over
+                gameOverRect.anchoredPosition = new Vector2(0, -Screen.height);
+                gameOverRect.gameObject.SetActive(true);
+                gameOverRect.DOAnchorPos(gameOverTargetPos, moveDuration)
+                    .SetEase(Ease.OutBack)
+                    .OnComplete(() =>
+                    {
+                        // show & animate score
+                        DOVirtual.DelayedCall(delayBetween, () =>
                         {
-                            replayButtonRect.gameObject.SetActive(true);
+                            var scoreText = finalScoreRect.GetComponent<TextMeshProUGUI>();
+                            if (scoreText != null) scoreText.text = $"Score: {finalScore}";
+                            finalScoreRect.anchoredPosition = new Vector2(0, -Screen.height);
+                            finalScoreRect.gameObject.SetActive(true);
+                            finalScoreRect.DOAnchorPos(scoreTargetPos, moveDuration)
+                                .SetEase(Ease.OutBack)
+                                .OnComplete(() =>
+                                {
+                                    replayButtonRect.gameObject.SetActive(true);
+                                });
                         });
-                });
+                    });
             });
     }
     
